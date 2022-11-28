776bcdda868f221f2440d4dd9bfd50676f1be53a
Andy <bungandy@gmail.com>
2022-11-28 11:35:05 +0700

    chore

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// import HelloWorld from './components/HelloWorld.vue'
-const title = import.meta.env.VITE_TITLE
-const logo = import.meta.env.VITE_LOGO
-let bg = import.meta.env.VITE_COLOR_BG
-if (!bg)
-  bg = "#f9f9f9f9"
+const {VITE_TITLE, VITE_LOGO, VITE_COLOR_BG, VITE_COLOR_TEXT, VITE_HEADING, VITE_CAPTION} = import.meta.env
 
-let text = import.meta.env.VITE_COLOR_TEXT
-if (!text)
-  text = "#222222"
+const title = VITE_TITLE && VITE_TITLE!==''                ? VITE_TITLE :  "Mineral"
 
-let heading = import.meta.env.VITE_HEADING
-if (!heading)
-  heading = "Be right back."
-
-let caption = import.meta.env.VITE_CAPTION
-if (!caption)
-  caption = `We're making updates to ${title}.<br/>Check back soon.`
+const env = {
+  "logo":     VITE_LOGO       && VITE_LOGO!==''             ? VITE_LOGO : "https://static.mineralcdn.net/site/mineralcoid/logo_mineral_black@3x.png",
+  "bg":       VITE_COLOR_BG   && VITE_COLOR_BG!==''     ? VITE_COLOR_BG : "#f9f9f9f9",
+  "text":     VITE_COLOR_TEXT && VITE_COLOR_TEXT!=='' ? VITE_COLOR_TEXT : "#222222",
+  "heading":  VITE_HEADING    && VITE_HEADING!==''       ? VITE_HEADING : "Be right back.",
+  "caption":  VITE_CAPTION    && VITE_CAPTION!==''       ? VITE_CAPTION : `We're making updates to ${title}.<br/>Check back soon.`
+}
 </script>
 
 <template>
-  <div class="w-screen h-screen grid place-items-center" :style="`background-color:${bg}; color:${text}`">
+  <div class="w-screen h-screen grid place-items-center" :style="`background-color:${env.bg}; color:${env.text}`">
     <div class="grid gap-y-4 place-items-center text-center">
-      <img :src="logo" width="100"/>
-      <h1 class="text-5xl font-bold" v-html="heading"></h1>
-      <p class="text-sm" v-html="caption"></p>
+      <img :src="env.logo" width="100"/>
+      <h1 class="text-4xl font-bold" v-html="env.heading"></h1>
+      <p class="text-sm leading-8" v-html="env.caption"></p>
     </div>
   </div>
 </template>
