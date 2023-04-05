08d5af3707f9e89eefe63f25a41e90803ae11985
Andy <bungandy@gmail.com>
2023-04-05 21:31:42 +0700

    change: be right back font size

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ const {VITE_TITLE, VITE_LOGO, VITE_COLOR_BG, VITE_COLOR_TEXT, VITE_HEADING, VITE
 const title = VITE_TITLE && VITE_TITLE!==''                ? VITE_TITLE :  "Mineral"
 
 const env = {
-  "logo":     VITE_LOGO       && VITE_LOGO!==''             ? VITE_LOGO : "https://static.mineralcdn.net/site/mineralcoid/logo_mineral_black@3x.png",
+  "logo":     VITE_LOGO       && VITE_LOGO!==''             ? VITE_LOGO : null,
   "bg":       VITE_COLOR_BG   && VITE_COLOR_BG!==''     ? VITE_COLOR_BG : "#f9f9f9f9",
   "text":     VITE_COLOR_TEXT && VITE_COLOR_TEXT!=='' ? VITE_COLOR_TEXT : "#222222",
   "heading":  VITE_HEADING    && VITE_HEADING!==''       ? VITE_HEADING : "Be right back.",
@@ -15,8 +15,8 @@ const env = {
 <template>
   <div class="w-screen h-screen grid place-items-center" :style="`background-color:${env.bg}; color:${env.text}`">
     <div class="grid gap-y-4 place-items-center text-center">
-      <img :src="env.logo" width="100"/>
-      <h1 class="text-4xl font-bold" v-html="env.heading"></h1>
+      <img v-if="env.logo" :src="env.logo" height="100"/>
+      <h1 class="text-xl font-bold" v-html="env.heading"></h1>
       <p class="text-sm leading-8" v-html="env.caption"></p>
     </div>
   </div>
